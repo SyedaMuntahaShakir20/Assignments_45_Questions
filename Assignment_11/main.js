f82e92faf3ffae1c03b5b1b8d20ecb0fc39a0b4f
Syeda Muntaha Shakir <164549986+SyedaMuntahaShakir20@users.noreply.github.com>
2024-06-08 11:53:50 -0700

    push Assignment 11

---
+++ Assignment_11/main.js
@@ -0,0 +1,11 @@
+"use strict";
+/* Exercise 12: Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them.
+The text of each message should be the same, but each message should be personalized with the person’s name. */
+let friendsList = ["Sana", "Arooba", "Hira", "Muneeba", "Ramsha"];
+let message = "hope you are fine? ";
+//Print text message with each friends
+console.log(friendsList[0] + " " + message);
+console.log(friendsList[1] + " " + message);
+console.log(friendsList[2] + " " + message);
+console.log(friendsList[3] + " " + message);
+console.log(friendsList[4] + " " + message);
---
+++ Assignment_11/main.ts
@@ -0,0 +1,13 @@
+/* Exercise 12: Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. 
+The text of each message should be the same, but each message should be personalized with the person’s name. */
+
+let friendsList : string[] = ["Sana", "Arooba","Hira","Muneeba","Ramsha"];
+let message : string = "hope you are fine? ";
+
+//Print text message with each friends
+console.log(friendsList[0] + " " + message);
+console.log(friendsList[1] + " " + message);
+console.log(friendsList[2] + " " + message);
+console.log(friendsList[3] + " " + message);
+console.log(friendsList[4] + " " + message);
+
