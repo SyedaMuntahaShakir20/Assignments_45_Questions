283d7604e3526ac552e050495c3e342042704f01
Syeda Muntaha Shakir <164549986+SyedaMuntahaShakir20@users.noreply.github.com>
2024-06-08 11:45:46 -0700

    push assignment 02

---
+++ Assignment_02/main.ts
@@ -0,0 +1,6 @@
+// Excerise 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName : string = "syeda Muntaha";
+console.log("LowerCase:", personName.toLowerCase());
+console.log("UpperCase:", personName.toUpperCase());
+console.log("TitleCase:", personName.replace(/\b\w/g, c=> c.toUpperCase()));
