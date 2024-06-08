208ab5568394146bde14356e74f1d89929590a6c
Syeda Muntaha Shakir <164549986+SyedaMuntahaShakir20@users.noreply.github.com>
2024-06-08 11:52:29 -0700

    push Assignment 09

---
+++ Assignment_09/main.js
@@ -0,0 +1,15 @@
+"use strict";
+/* Exercise 10: Adding Comments:
+Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does. */
+//Author: [Syeda Muntaha Shakir]
+//Date : [14-04-2024]
+//Storing My Favourite Person Name in a variable
+let personName = "Syed Shakir";
+//print my favourite person name
+console.log(`Hello ${personName}, would you like to learn some python`);
+//Author: [Syeda Muntaha Shakir]
+//Date : [14-04-2024]
+//Storing my favourite Number in a variable
+let favNumber = 9;
+//print my favourite number
+console.log(`My favourite Number is ${favNumber}`);
---
+++ Assignment_09/main.ts
@@ -0,0 +1,20 @@
+/* Exercise 10: Adding Comments: 
+Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does. */
+
+//Author: [Syeda Muntaha Shakir]
+//Date : [14-04-2024]
+//Storing My Favourite Person Name in a variable
+
+let personName : string = "Syed Shakir";
+
+//print my favourite person name
+console.log(`Hello ${personName}, would you like to learn some python`);
+
+//Author: [Syeda Muntaha Shakir]
+//Date : [14-04-2024]
+//Storing my favourite Number in a variable
+
+let favNumber : number = 9
+
+//print my favourite number
+console.log(`My favourite Number is ${favNumber}`);
