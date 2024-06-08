07059dc4466f2b94b68d9333c382c2199c316e82
Syeda Muntaha Shakir <164549986+SyedaMuntahaShakir20@users.noreply.github.com>
2024-06-08 11:51:53 -0700

    push Assignment 08

---
+++ Assignment_08/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favNumber = 9;
+//print your favourite number
+console.log(`My favourite Number is ${favNumber}`);
---
+++ Assignment_08/main.ts
@@ -0,0 +1,7 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favNumber : number = 9
+
+//print your favourite number
+console.log(`My favourite Number is ${favNumber}`);
+
