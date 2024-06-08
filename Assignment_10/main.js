f23fc7941d6a0b3c3406c66b4927357b07d05166
Syeda Muntaha Shakir <164549986+SyedaMuntahaShakir20@users.noreply.github.com>
2024-06-08 11:53:02 -0700

    push Assignment 010

---
+++ Assignment_10/main.js
@@ -0,0 +1,10 @@
+"use strict";
+/* Exercise 11:
+Names: Store the names of a few of your friends in a array called names.
+Print each person’s name by accessing each element in the list, one at a time. */
+let friendsList = ["Sana", "Arooba", "Hira", "Muneeba", "Ramsha"];
+console.log(friendsList[0]);
+console.log(friendsList[1]);
+console.log(friendsList[2]);
+console.log(friendsList[3]);
+console.log(friendsList[4]);
---
+++ Assignment_10/main.ts
@@ -0,0 +1,14 @@
+/* Exercise 11:
+Names: Store the names of a few of your friends in a array called names.
+Print each person’s name by accessing each element in the list, one at a time. */
+
+let friendsList : string[] = ["Sana", "Arooba","Hira","Muneeba","Ramsha"];
+
+//print array in a List.
+
+console.log(friendsList[0]);
+console.log(friendsList[1]);
+console.log(friendsList[2]);
+console.log(friendsList[3]);
+console.log(friendsList[4]);
+
