cb462596bdae4c1ef50b2a811750f21c9a9e965e
Syeda Muntaha Shakir <164549986+SyedaMuntahaShakir20@users.noreply.github.com>
2024-06-08 11:48:27 -0700

    push Assignment 03

---
+++ Assignment_03/main.js
@@ -0,0 +1,6 @@
+"use strict";
+/* Exercise 04: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.” */
+let quote = "Injustice anywhere is a threat to justice everywhere.";
+let famousPerson = "Martin Luther King Jr";
+console.log(`${famousPerson} once said, "${quote}"`);
